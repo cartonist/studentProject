e82668df05c913e53895a5d87f69e82d72a26a3e
cartonist <3318279402@qq.com>
2023-04-21 23:59:23 +0800

    修改了服务器的ip

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
-	host: 'localhost',
+	host: '10.107.161.70',
 	user: 'root',
 	password: 'LXL1314520',
 	database: 'student_project'
